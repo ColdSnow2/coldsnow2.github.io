086d706c1e043e07b276d97f58d3a463f3919fa7
James Henry <99589107+ColdSnow2@users.noreply.github.com>
2022-03-11 18:05:47 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+<html lang="en">
+  <head>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/assets/style001.css">
+  </head>
+  <body>
+    <div class="top-menu">
+      <div class="left-items-menu">
+        <a href="coldsnow2.github.io/discord">Discord</a>
+        <a href="coldsnow2.github.io/authorize/user/hensocket">Create new HenSocket (Account)</a>
+        <a href="coldsnow2.github.io/discord">Discord</a>
+        <a href="coldsnow2.github.io/discord">Discord</a>
+       </div>
+    </div>
+  </body>
+</html>
